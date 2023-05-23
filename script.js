c476eff68781c5feef0124181c9269d904bf742c
Khijar Sheikh <120732221+Khijar21@users.noreply.github.com>
2023-05-23 15:25:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function highlight() {
-    //Write your code here
+let bold;
+window.onload = getBoldElement();
 
-
+function getBoldElement(){
+	bold = document.getElementsByTagName('strong');
 }
 
-
+function highlight() {
+	for(let i = 0; i < bold.length; i++){
+		bold[i].style.color = "green";
+	}
+}
 function return_normal() {
     //Write your code here
-
-    
+	for(let i = 0; i < bold.length; i++){
+		bold[i].style.color= "black";
+	}
 }
